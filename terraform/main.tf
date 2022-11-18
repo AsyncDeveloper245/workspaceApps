1547f0b8ddd6c2de6f2b25a6b7276483ff0797da
AsyncDeveloper245 <AsyncDeveloper245@users.noreply.github.com>
2022-11-18 19:54:32 +0100

    add ansible,tf config

--- terraform/main.tf
+++ terraform/main.tf
@@ -70,7 +70,6 @@ resource "aws_instance" "public_instance" {
   ami                         = data.aws_ami.devc_data_source.id
   instance_type               = "t2.micro"
   subnet_id                   = aws_subnet.devc_public_subnet.id
-  key_name                    = aws_key_pair.devc_auth.id
   vpc_security_group_ids      = [aws_security_group.public_sg.id]
   associate_public_ip_address = true
   key_name                   = "hng-pk"
